0f109ffc234347bb8529d552565ed01bbef262bc
Iselin J. Nordstrøm-Hauge <iselinjn@stud.ntnu.no>
2021-03-08 10:26:31 +0100

    first draft elevator

--- driver-go-master/elevio/elevator.go
+++ driver-go-master/elevio/elevator.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
  // Enum definitions
+
+var _numButtons int = 3 // Fra elevator_io_types.h
+
 type ElevatorBehaviour int
 
 const (
@@ -24,7 +27,7 @@ const (
 type Elevator struct {
 	floor int
 	motorDirection MotorDirection
-	//requests[N_FLOORS][N_BUTTONS]
+	requests[_numFloors][_numButtons] // Hvor kommer denne fra? Får feil
 	behaviour ElevatorBehaviour
 	config struct{
 		clearRequestVariant ClearRequestVariant
@@ -35,22 +38,22 @@ type Elevator struct {
 
 // Implementering av funksjoner (C-fil)
 
-/* Med channel
-func ebToString(elevBehav chan<- ElevatorBehaviour) string {
-	behaviour := ""
+func ebToString(elevBehav chan<- ElevatorBehaviour) string { // Trenger vi å importere? Stor forbokstav!
+	behav := ""
 	select {
 	case elevBehav<-EB_Idle:
-		behaviour = "ED_Idle"
+		behav = "ED_Idle"
 	case elevBehav<-EB_DoorOpen:
-		behaviour = "EB_DoorOpen"
+		behav = "EB_DoorOpen"
 	case elevBehav<-EB_Moving:
-		behaviour = "EB_Moving"
+		behav = "EB_Moving"
 	default:
-		behaviour = "EB_Undefined"
+		behav = "EB_Undefined"
 	}
-	return behaviour // Returne her eller i hver case?
+	return behav // Returne her eller i hver case?
 }
-*/
+
+/* Uten channels
 func ebToString(elevBehav ElevatorBehaviour) string {
 	switch elevBehav {
 	case EB_Idle:
@@ -61,9 +64,9 @@ func ebToString(elevBehav ElevatorBehaviour) string {
 		return "EB_Moving"
 	}
 	return "EB_Undefined"
-}
+}*/
 
-func elevioMotorDirToString(mDirection MotorDirection) string {
+func elevioMotorDirToString(mDirection MotorDirection) string { // Importere? Stor forbokstav!
 	switch mDirection {
 	case MD_Stop:
 		return "MD_Stop"
@@ -76,30 +79,40 @@ func elevioMotorDirToString(mDirection MotorDirection) string {
 }
 
 func elevatorPrint(es Elevator) {
-	fmt.Println("  +--------------------+\n") // Sjekk om dette er riktig print-funksjon!
-	fmt.Println("  |floor = %-2d          |\n  |directin  = %-12.12s|\n |behaviour = %-12.12s|\n",
-		es.floor, elevioMotorDirToString(es.motorDirection), ebToString(es.behaviour))
-	fmt.Println("  +--------------------+\n")
-	fmt.Println("  |  | up  | dn  | cab |\n")
-	/*for(int f = N_FLOORS-1; f >= 0; f--){
-		printf("  | %d", f);
-		for(int btn = 0; btn < N_BUTTONS; btn++){
-			if((f == N_FLOORS-1 && btn == B_HallUp)  ||
-				(f == 0 && btn == B_HallDown)
-		){
-				printf("|     ");
+	fmt.Printf("  +--------------------+\n") // Sjekk om dette er riktig print-funksjon!
+	fmt.Printf("  |floor = %-2d          |\n",es.floor) // - i %2-d betyr bare - at teksten er left-justified (kosmetisk)
+	fmt.Printf("|direction  = %-12.12s|\n", elevioMotorDirToString(es.motorDirection)) // Måtte dele opp for at det skulle bli riktig
+	fmt.Print("|behaviour = %-12.12s|\n", ebToString(es.behaviour) // Hvorfor feilmelding her?
+	fmt.Printf("  +--------------------+\n")
+	fmt.Printf("  |  | up  | dn  | cab |\n")
+
+	for f := _numFloors-1; f >= 0; f-- {
+		fmt.Printf("  | %d", f)
+		for btn := 0; btn < _numButtons; btn++{
+			if (f == _numFloors-1 && btn == BT_HallUp) || (f == 0 && btn == BT_HallDown){ // Hvorfor blir BT_HallUp feil når ButtonType er int?
+				fmt.Printf("|     ")
 			} else {
-				printf(es.requests[f][btn] ? "|  #  " : "|  -  ");
+				if es.requests[f][btn] == true { // Fint om vi finner en bedre måte å skrive denne på
+					fmt.Printf("|  #  ")
+				} else{
+					fmt.Printf("|  -  ")
+				}
+				// fmt.Printf(es.requests[f][btn] ? "|  #  " : "|  -  "); Originale linjen, disse operatorene finnes ikke i go
 			}
 		}
-		printf("|\n");
+		fmt.Printf("|\n");
 	}
-	printf("  +--------------------+\n");*/
+	fmt.Printf("  +--------------------+\n");
 }
 
 
-/*
-func elevatorUnitialized() Elevator {
-	...
+
+func elevatorUnitialized() Elevator { // Helt klart feil, må finne ut hvordan denne løses
+	Elevator.floor = -1
+	Elevator.motorDirection = MD_Stop
+	Elevator.behaviour = EB_Idle
+	Elevator.config.clerRequestVariant = CV_All
+	Elevator.config.doorOpenDurationSec = 3.0
+	return Elevator.floor, Elevator.motorDirection, Elevator.behaviour,
+	Elevator.config.clerRequestVariant, Elevator.config.doorOpenDurationSec
 }
-*/
