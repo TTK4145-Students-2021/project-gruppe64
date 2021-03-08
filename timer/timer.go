308a4a0d641441cacc8194c76d71f33c8d994e99
Hanne-Grete Alvheim <hannegga@stud.ntnu.no>
2021-03-08 12:29:49 +0100

    removed global variables, added channels and goroutines. Added a main-function to see if the timer did what it should do.

--- timer/timer.go
+++ timer/timer.go
@@ -1,29 +1,70 @@
-package timer
-import "time"
+package main
 
-func get_wall_time() float64{
-	time_now := time.Now()
-	time_sec := float64(time_now.Second())
-	nano := float64(time_now.UnixNano())
-	nano = nano*0.000000001
-	time_out := time_sec+nano
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-	return time_out //returning current time in seconds and nanoseconds I think, it is probably wrong since it is in int64.
+
+//getWallTime returns the current time in milliseconds
+func getWallTime() float64{
+	timeNow := time.Now()
+	//timeSec := float64(timeNow.Second())
+	nano := float64(timeNow.UnixNano())
+	milli := nano/1000000 //milliseconds
+	return milli
 }
 
-var timerEndtime = 0.00
-var timerActive = false
-
-func timerStart (duration float64){
-	timerEndtime = get_wall_time() + duration
-	timerActive = true
+//timerStart takes in starts a timer , and puts the endtime for that timer into a channel, timerEndTime. It also states
+//that timerActive is true.
+func timerStart (duration float64, timerActive chan <- bool, timerEndTime chan <- float64){
+	//defer wg.Done()
+	timerEndTime <- getWallTime() + duration //duration must here be given in milliseconds.
+	timerActive <- true
 }
 
-func timerStop(){
-	timerActive = false
+//timerStop is used when we want the timer to stop being active.
+func timerStop(timerActive chan <- bool, timerEndTime chan <- float64){
+	timerActive <- false
+	timerEndTime <- 0.00
+	//word := <- timerActive
+	//fmt.Println(word)
 }
 
-func timer_timedOut() bool{
-	return timerActive && get_wall_time()>timerEndtime
+
+//timerTimedOut is used as to find out whether we have surpassed the duration-limit. If we have, it returns true.
+func timerTimedOut(wg *sync.WaitGroup,timerActive chan bool, timerEndTime  chan float64){ //bool{
+	defer wg.Done()
+	endTime := <-timerEndTime
+	active := <- timerActive
+	for{
+		if active == true {
+			if getWallTime() > endTime{
+				fmt.Println("Too long time")
+				//can also just return true here, if we don't want to have a for loop but poll instead. Thought for loop
+				//was best for goroutines.
+				break
+			}
+
+		}
+	}
+	timerStop(timerActive, timerEndTime)
+}
+
+
+func main(){
+	var wg sync.WaitGroup
+	timerActive := make(chan bool,1)
+	timerEndTime := make(chan float64,1)
+	timerStart(5000, timerActive, timerEndTime)
+	wg.Add(1)
+	go timerTimedOut(&wg, timerActive, timerEndTime) //is 1 sec slower than what is written in timer, e.g. if 5000 ms is written
+	// in timerStart it uses about 6 secs. Much slower if we use seconds instead of milliseconds though.
+	wg.Wait() //used to wait for the goroutine to finish
+
+	<-timerActive
+	<-timerEndTime
+
 }
 
