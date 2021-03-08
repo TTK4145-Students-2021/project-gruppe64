5b47fd960e2a9bf15392213ada1e926391dede59
Hanne-Grete Alvheim <hannegga@stud.ntnu.no>
2021-03-08 13:30:41 +0100

    Tried having several timers in main, stopped using timerStop

--- timer/timer.go
+++ timer/timer.go
@@ -27,29 +27,27 @@ func timerStart (duration float64, timerActive chan <- bool, timerEndTime chan <
 //timerStop is used when we want the timer to stop being active.
 func timerStop(timerActive chan <- bool, timerEndTime chan <- float64){
 	timerActive <- false
-	timerEndTime <- 0.00
-	//word := <- timerActive
-	//fmt.Println(word)
 }
 
 
 //timerTimedOut is used as to find out whether we have surpassed the duration-limit. If we have, it returns true.
-func timerTimedOut(wg *sync.WaitGroup,timerActive chan bool, timerEndTime  chan float64){ //bool{
+func timerTimedOut(duration float64, wg *sync.WaitGroup,timerActive chan bool, timerEndTime  chan float64) { //bool{
 	defer wg.Done()
+	timerStart(duration, timerActive, timerEndTime)
 	endTime := <-timerEndTime
 	active := <- timerActive
 	for{
 		if active == true {
 			if getWallTime() > endTime{
 				fmt.Println("Too long time")
+				break
 				//can also just return true here, if we don't want to have a for loop but poll instead. Thought for loop
 				//was best for goroutines.
-				break
 			}
 
 		}
 	}
-	timerStop(timerActive, timerEndTime)
+	//timerStop(timerActive, timerEndTime)
 }
 
 
@@ -57,14 +55,24 @@ func main(){
 	var wg sync.WaitGroup
 	timerActive := make(chan bool,1)
 	timerEndTime := make(chan float64,1)
-	timerStart(5000, timerActive, timerEndTime)
-	wg.Add(1)
-	go timerTimedOut(&wg, timerActive, timerEndTime) //is 1 sec slower than what is written in timer, e.g. if 5000 ms is written
-	// in timerStart it uses about 6 secs. Much slower if we use seconds instead of milliseconds though.
-	wg.Wait() //used to wait for the goroutine to finish
+	//timerStart(5000, timerActive, timerEndTime)
+	for i := 0; i < 30; i++{
+		wg.Add(1)
+		go timerTimedOut(float64(2000), &wg, timerActive, timerEndTime)
 
-	<-timerActive
-	<-timerEndTime
+	}
+
+	wg.Wait()
+	close(timerActive)
+	close(timerEndTime)
+	fmt.Println("Finished process")
+
+	//wg.Add(1)
+	//go timerTimedOut(1000, &wg, timerActive, timerEndTime) //is 1 sec slower than what is written in timer, e.g. if 5000 ms is written
+	// in timerStart it uses about 6 secs. Much slower if we use seconds instead of milliseconds though.
+	//wg.Add(1)
+	//go timerTimedOut(4000,&wg, timerActive, timerEndTime)
+	//wg.Wait() //used to wait for the goroutine to finish
 
 }
 
