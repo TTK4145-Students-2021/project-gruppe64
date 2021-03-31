0f72b09c87f3eb92d372e2dd9ca6777cac1a7076
mknh <59824798+mknh@users.noreply.github.com>
2021-03-31 15:17:50 +0200

    Continued handling json

--- distributor/distributor.go
+++ distributor/distributor.go
@@ -18,6 +18,9 @@ const (
 )
 
 func initiateElevatorsOnNet() [3]elevatorIdentity {
+	elevOneNums := []int{19001, 19998, 20001} // nr. 1 for IP-adresses, 2 for elevstructs to this, 3 for orders to this
+	elevTwoNums := []int{19002, 19999, 20002}
+	elevThreeNums := []int{19003, 20000, 20002}
 	var elevs [3]elevatorIdentity
 	elevs[0] = elevatorIdentity{19001, 19998, 19999, fsm.Elevator{}}
 	elevs[1] = elevatorIdentity{19002, 19999, 20002, fsm.Elevator{}}
@@ -33,15 +36,20 @@ type elevators struct{
 			behaviour string `json:"behaviour"`
 			floor int `json:"floor"`
 			motorDirection string  `json:"direction"`
-			orders []bool `json: ""`
-
-		}
+			orders []bool `json: "cabRequests"`
+		} `json: "one"`
 		elevatorTwo struct{
-
-		}
+			behaviour string `json:"behaviour"`
+			floor int `json:"floor"`
+			motorDirection string  `json:"direction"`
+			orders []bool `json: "cabRequests"`
+		} `json: "two"`
 		elevatorThree struct{
-
-	}
+			behaviour string `json:"behaviour"`
+			floor int `json:"floor"`
+			motorDirection string  `json:"direction"`
+			orders []bool `json: "cabRequests"`
+		}`json: "three"`
 	}
 }
 
