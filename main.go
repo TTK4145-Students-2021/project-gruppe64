b2a2794bd95be6e8a86e3a79e70ac29fa1e1c3d6
mknh <59824798+mknh@users.noreply.github.com>
2021-04-21 13:21:34 +0200

    Init order fixed

--- main.go
+++ main.go
@@ -41,58 +41,51 @@ func primaryWork(activateAsPrimary <-chan bool){
 				orderToSelfCh := make(chan system.ButtonEvent)
 				doorTimerTimedOutCh := make(chan bool)
 
-
 				// ->Distributor
 				hallOrderCh := make(chan system.ButtonEvent)
 				ownElevatorCh := make(chan system.Elevator)
+				otherElevatorCh := make(chan system.Elevator)
 				messageTimerTimedOutCh := make(chan system.NetOrder)
 				orderTimerTimedOutCh := make(chan system.NetOrder)
-				otherElevatorCh := make(chan system.Elevator)
+				elevatorConnectedCh := make(chan int, system.NumElevators - 1)
+				elevatorDisconnectedCh := make(chan int, system.NumElevators - 1)
 
 				// ->Network
+				shareOwnElevatorCh := make(chan system.Elevator)
+				orderThroughNetCh := make(chan system.NetOrder)
 
 
-				//->Timer
+				// ->Timer
 				doorTimerDurationCh := make(chan float64)
 				messageTimerCh := make(chan system.NetOrder)
 				placedMessageReceivedCh := make(chan system.NetOrder)
 				orderTimerCh := make(chan system.NetOrder)
 
-
-				//Network channels for transmitting and receiving
-				shareOwnElevatorCh := make(chan system.Elevator)
-				elevatorConnectedCh := make(chan int, system.NumElevators - 1)
-				elevatorDisconnectedCh := make(chan int, system.NumElevators - 1)
-				orderThroughNetCh := make(chan system.NetOrder)
-
-
-
-
-				// Timers:
-				go timer.RunDoorTimer(doorTimerDurationCh, doorTimerTimedOutCh)
-				go timer.RunMessageTimer(messageTimerCh, placedMessageReceivedCh, messageTimerTimedOutCh)
-				go timer.RunOrderTimer(orderTimerCh, orderTimerTimedOutCh)
-
 				// Hardware:
 				go hardwareIO.RunHardware(orderToSelfCh, hallOrderCh, floorArrivalCh, obstructionEventCh)
-
-				// Distributor:
-				go fsm.ElevatorFSM(orderToSelfCh, floorArrivalCh, obstructionEventCh, ownElevatorCh,
-					doorTimerDurationCh, doorTimerTimedOutCh)
 
 				// FSM:
 				go distributor.OrderDistributor(hallOrderCh, otherElevatorCh, ownElevatorCh, shareOwnElevatorCh,
 					orderThroughNetCh, orderToSelfCh, messageTimerCh, messageTimerTimedOutCh, orderTimerCh,
 					orderTimerTimedOutCh, elevatorConnectedCh, elevatorDisconnectedCh)
 
+				// Distributor:
+				go fsm.ElevatorFSM(orderToSelfCh, floorArrivalCh, obstructionEventCh, ownElevatorCh,
+					doorTimerDurationCh, doorTimerTimedOutCh)
+
 				// Network:
 				go sendandreceive.RunNetworking(shareOwnElevatorCh, otherElevatorCh, orderThroughNetCh,
 					placedMessageReceivedCh, orderToSelfCh, elevatorConnectedCh, elevatorDisconnectedCh)
+
+				// Timers:
+				go timer.RunDoorTimer(doorTimerDurationCh, doorTimerTimedOutCh)
+				go timer.RunMessageTimer(messageTimerCh, placedMessageReceivedCh, messageTimerTimedOutCh)
+				go timer.RunOrderTimer(orderTimerCh, orderTimerTimedOutCh)
+
 			}
 		}
 	}
 }
-
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
